2d6169ba29bb00e8448ccbfec3d8f92b3f5f49b3
presiannedyalkov <presiannedyalkov@users.noreply.github.com>
2016-01-22 14:49:20 +0100

    fixing js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function() {
    jQuery(".js_terms").click(function() {
-       jQuery(".container").prepend(".text-box");
+       jQuery(".container").append(".text-box");
        jQuery(".text-box").append(".terms-text")
    });
-   jQuery(".js_terms").click(function() {
-       jQuery(".container").prepend(".text-box");
+   jQuery(".js_help").click(function() {
+       jQuery(".container").append(".text-box");
        jQuery(".text-box").append(".help-text")
    });
 });

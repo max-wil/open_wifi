d0866f6f2cd435560f6549bd21936a0631e545e8
presiannedyalkov <presiannedyalkov@users.noreply.github.com>
2016-01-22 15:23:56 +0100

    js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 jQuery(document).ready(function() {
-    var textBox = jQuery.add(div).addClass(".text-box");
+    var textBox = jQuery(".text-box");
     var termsText = jQuery(".terms-text");
     var helpText = jQuery(".help-text");
     
    jQuery(".js_terms").click(function() {
-       jQuery(".container").append(textBox);
+       jQuery(".container").append("<div class='text-box'>");
        jQuery(".text-box").append(termsText);
    });
    jQuery(".js_help").click(function() {
        jQuery(".container").append(textBox);
-       jQuery(".text-box").append(helpText);
+       jQuery(".text-box").append();
    });
 });
